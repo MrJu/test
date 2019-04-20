ce9dd06fd000eea8de4694b1e46079cda894d962
andrew <mrju.email@gmail.com>
2019-04-21 03:16:10 +0800

    simple module
    Signed-off-by: andrew <mrju.email@gmail.com>

---
+++ 001/foo.c
@@ -0,0 +1,32 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/printk.h>
+
+#define STR(x) _STR(x)
+#define _STR(x) #x
+
+#define MAJOR_VERSION 0
+#define MINOR_VERSION 0
+#define PATCH_VERSION 0
+
+#define VERSION STR(MAJOR_VERSION.MINOR_VERSION.PATCH_VERSION)
+
+static int __init foo_init(void)
+{
+	printk("%s(): %d\n", __func__, __LINE__);
+	return 0;
+}
+
+static void __exit foo_exit(void)
+{
+	printk("%s(): %d\n", __func__, __LINE__);
+}
+
+module_init(foo_init);
+module_exit(foo_exit);
+
+MODULE_AUTHOR("andrew, mrju.email@gmail.com");
+MODULE_DESCRIPTION("Linux is not Unix");
+MODULE_VERSION(VERSION);
+MODULE_LICENSE("GPL");
+MODULE_ALIAS("foo-driver");
